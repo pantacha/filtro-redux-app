3900b9c1914d3fbd2d115bbb9c54fa02cdfd7113
pantacha <pantacha@hotmail.com>
2019-07-23 22:06:29 +0200

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//ngrx
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import {appReducers} from './app.reducers';
+
+import {ReactiveFormsModule} from '@angular/forms';
+
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { TodoAddComponent } from './todo/todo-add/todo-add.component';
+import { TodoFooterComponent } from './todo/todo-footer/todo-footer.component';
+import { TodoItemComponent } from './todo/todo-item/todo-item.component';
+import { TodoListComponent } from './todo/todo-list/todo-list.component';
+import { TodoComponent } from './todo/todo.component';
+import { environment } from '../environments/environment';
+import { from } from 'rxjs';
+import { FilterPipe } from './fiter/filter.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    TodoAddComponent,
+    TodoFooterComponent,
+    TodoItemComponent,
+    TodoListComponent,
+    TodoComponent,
+    FilterPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    StoreModule.forRoot(appReducers),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
